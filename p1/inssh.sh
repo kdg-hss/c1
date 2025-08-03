c24313aa2ebeaab47e603f0eb1fc65d20c00137d
Imis Bagus <137598019+kdg-hss@users.noreply.github.com>
2025-08-03 16:05:05 +0800

    Update inssh.sh

--- p1/inssh.sh
+++ p1/inssh.sh
@@ -8,8 +8,16 @@
 # //====================================================
 # // font color configuration | JULAK BANTUR AUTOSCRIPT
 clear
-red() { echo -e "\\033[32;1m${*}\\033[0m"; }
-clear
+red='\e[1;31m'
+green='\e[0;32m'
+yell='\e[1;33m'
+tyblue='\e[1;36m'
+NC='\e[0m'
+purple() { echo -e "\\033[35;1m${*}\\033[0m"; }
+tyblue() { echo -e "\\033[36;1m${*}\\033[0m"; }
+yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
 #######AUTOSCRIPT JULAK VPN VERSI 0.1#######
 julak="https://raw.githubusercontent.com/kdg-hss/c1/main/"
 #######AUTOSCRIPT JULAK VPN VERSI 0.1#######

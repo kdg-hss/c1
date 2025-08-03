3c5a4127715c2d9302359c75f11b8fb74a65a703
Imis Bagus <137598019+kdg-hss@users.noreply.github.com>
2025-08-03 16:10:08 +0800

    Create inws.sh

---
+++ p1/inws.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+# //====================================================
+# //	System Request:Debian 9+/Ubuntu 18.04+/20+
+# //	Author:	Julak Bantur
+# //	Dscription: Xray MultiPort
+# //	email: putrameratus2@gmail.com
+# //  telegram: https://t.me/Cibut2d
+# //====================================================
+# // font color configuration | JULAK BANTUR AUTOSCRIPT
+clear
+red='\e[1;31m'
+green='\e[0;32m'
+yell='\e[1;33m'
+tyblue='\e[1;36m'
+NC='\e[0m'
+purple() { echo -e "\\033[35;1m${*}\\033[0m"; }
+tyblue() { echo -e "\\033[36;1m${*}\\033[0m"; }
+yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+#######AUTOSCRIPT JULAK VPN VERSI 0.1#######
+julak="https://raw.githubusercontent.com/kdg-hss/c1/main/"
+#######AUTOSCRIPT JULAK VPN VERSI 0.1#######
+clear
+apt update
+apt install python3 -y
+apt install python3-pip -y
+apt install python3-requests -y
+
+mkdir -p /etc/websocket
+
+wget -q -O /etc/websocket/ws.py "${julak}/s3/ws.py"
+chmod +x /etc/websocket/ws.py
+
+
+# Installing Service
+cat > /etc/systemd/system/ws.service << END
+[Unit]
+Description=Websocket By julak bantur
+Documentation=https://google.com
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/bin/python3 -O /etc/websocket/ws.py 10015
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+END
+
+systemctl daemon-reload
+systemctl enable ws.service
+systemctl start ws.service
+systemctl restart ws.service
+
+# Installing Service
+cat > /etc/systemd/system/ws-ovpn.service << END
+[Unit]
+Description=OpenVPN By Julak Bantur
+Documentation=https://google.com
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/bin/python3 -O /etc/websocket/ws.py 10012
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+END
+
+systemctl daemon-reload
+systemctl enable ws-ovpn
+systemctl start ws-ovpn
+systemctl restart ws-ovpn
